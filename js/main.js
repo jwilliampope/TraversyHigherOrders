6185dfa67ef6a8c537c581a4e545725f94cc3aba
jwilliampope <jwilliampope@hotmail.com>
2021-02-16 08:09:35 -0500

    added for loop heading

--- js/main.js
+++ js/main.js
@@ -18,9 +18,13 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32]
 
 // FOR EACH
 
+// a traditional for loop:
+
 // for (let i = 0; i < companies.length; i++) {
 //   console.log(companies[i])
 // }
+
+// for each
 
 // companies.forEach(function (company) {
 //   console.log(company.name)
